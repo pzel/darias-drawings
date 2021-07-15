366f19fcb37976fd403b25d3bade150c2e10626a
Simon Zelazny <zelazny@mailbox.org>
2021-07-15 16:27:41 +0200

    rotate some pictures

--- generate.rb
+++ generate.rb
@@ -2,10 +2,12 @@
 # just run me: ./generate.rb
 require "erb"
 template = ERB.new(File.read("index.erb"), nil, '-') #trim empty with -%>
-rotated = %w[ IMG_0682.JPG IMG_1083.JPG IMG_1120.JPG IMG_1121.JPG IMG_1127.JPG IMG_1128.JPG IMG_1130.JPG ]
+flipped = %w[ 894BF616-4304-4426-B9DB-73B8267286D8.jpg IMG_0980.JPG IMG_1106.JPG IMG_1122.JPG IMG_1123.JPG IMG_1124.JPG IMG_1697.JPG ]
+tipped = %w[ ]
 
 images = `ls -tr -1 | grep 'JPG\\|JPEG\\|jpg\\|jpeg'`.lines.map{ |filename|
-  {rotate: rotated.include?(filename.strip),
+  {flip: flipped.include?(filename.strip),
+    tip: tipped.include?(filename.strip),
     src:  filename.strip}
 }
 
